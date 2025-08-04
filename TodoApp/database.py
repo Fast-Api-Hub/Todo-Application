da73a617f99c494f2b552763d0e72d872c22d19c
Efran Himel <efranhimel25@gmail.com>
2025-08-04 10:25:28 -0400

    added main to start app

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLACLEHEMY_DATABASE_URL = "sqlite://./todos.db"
+SQLACLEHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 # FASTAPI can communicate with db via multiple threads, thus don't want to limit to one thread
 engine = create_engine(SQLACLEHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
